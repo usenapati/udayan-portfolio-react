d38665783d406d938c969b68e04c4495492423c2
Udayan Senapati <udayansenapati@gmail.com>
2022-11-07 21:35:11 -0500

    Fixed warnings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+// src/components/Contact.js
+
 import React from "react";
 
 export default function Contact() {
@@ -18,7 +21,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
+      body: encode({ "form-name": "test", name, email, message }),
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
@@ -65,7 +68,7 @@ export default function Contact() {
         </div>
         <form
           netlify
-          name="contact"
+          name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
