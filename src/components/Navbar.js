7102efd6774948eeeb8a2673c98c9c24d489c08b
Udayan Senapati <udayansenapati@gmail.com>
2022-11-07 21:39:05 -0500

    Fix Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="/about" className="ml-3 text-xl">
+          <a href="/#about" className="ml-3 text-xl">
             Udayan Senapati
           </a>
         </a>
@@ -17,14 +17,14 @@ export default function Navbar() {
           <a href="/projects" className="mr-5 hover:text-white">
             Past Work
           </a>
-          <a href="/skills" className="mr-5 hover:text-white">
+          <a href="/#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="/testimonials" className="mr-5 hover:text-white">
+          <a href="/#testimonials" className="mr-5 hover:text-white">
             Testimonials
           </a>
         </nav>
-        <a href="/contact"
+        <a href="/#contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
